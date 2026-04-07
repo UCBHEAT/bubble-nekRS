d31a8af9f7ceb94a062a8a99e89a0a4c3573abba
Benjamin James Li <ben_li@berkeley.edu>
2026-04-07 09:08:01 -0700

    gpu: 3D: calculate deltael, eps_cls dynamically

--- gpu/3D/customhooks.hpp
+++ gpu/3D/customhooks.hpp
@@ -7,17 +7,17 @@ void customProperties(double t)
     // Properties for momentum transport equation
     const occa::memory o_psi = scalar->o_solution("psi");
     // mu = ((1.0-psi)*muratio + psi)/Re
-    weightedMixing(mesh->Nelements, o_psi, muratio, Re, fluid->o_diffusionCoeff());
+    weightedMixing(info, o_psi, muratio, Re, fluid->o_diffusionCoeff());
     // rho = (1.0-psi)*rhoratio + psi
-    weightedMixing(mesh->Nelements, o_psi, rhoratio, 1.0, fluid->o_transportCoeff());
+    weightedMixing(info, o_psi, rhoratio, 1.0, fluid->o_transportCoeff());
 
     // Properties for species transport equation
     // D = (psi/(psi+(1.0-psi)*solubilityratio) +
     //     diffratio*(1.0-psi)/((1.0-psi)+psi/solubilityratio))/Pe
-    speciesDiff(mesh->Nelements, o_psi, solubilityratio, diffratio, Pe,
+    speciesDiff(info, o_psi, solubilityratio, diffratio, Pe,
             scalar->o_diffusionCoeff("c"));
     // No transport coefficient in c equation.
-    constantFill(mesh->Nelements, 1.0, scalar->o_transportCoeff("c"));
+    constantFill(info, 1.0, scalar->o_transportCoeff("c"));
 }
 
 void customSource(double t)
@@ -36,11 +36,11 @@ void customSource(double t)
 
     // Calculate interface unit normals.
     opSEM::strongGrad(mesh, nrs->fieldOffset, o_phi, o_cstVector);
-    interfaceNormals(mesh->Nelements, o_phi, o_psi, o_cstVectorX, o_cstVectorY, o_cstVectorZ);
+    interfaceNormals(info, o_phi, o_psi, o_cstVectorX, o_cstVectorY, o_cstVectorZ);
     scalar->o_solution("debug1").copyFrom(o_cstVectorY);
 
     // Calculate CST vector field.
-    speciesSource(mesh->Nelements, o_c, o_psi, solubilityratio, diffratio, Pe,
+    speciesSource(info, o_c, o_psi, solubilityratio, diffratio, Pe,
         o_cstVectorX, o_cstVectorY, o_cstVectorZ);
     scalar->o_solution("debug2").copyFrom(o_cstVectorY);
 
@@ -49,5 +49,5 @@ void customSource(double t)
     scalar->o_solution("debug3").copyFrom(scalar->o_explicitTerms("c"));
 
     // Buoyancy source term for the U equation.
-    weightedMixing(mesh->Nelements, o_psi, 1e64, 1e64*Fr*Fr*rhoratio, o_uSourceY);
+    weightedMixing(info, o_psi, 1e64, 1e64*Fr*Fr*rhoratio, o_uSourceY);
 }
--- gpu/3D/util.hpp
+++ gpu/3D/util.hpp
@@ -1,30 +1,5 @@
 #include <cmath>
 
-// Must be set to the average element length.
-// TODO: calculate from mesh on-demand, as in
-// https://github.com/nandu90/Nek5000/blob/nekLS/core/experimental/lvlSet.f#L587
-const double deltael = 0.08333333333;
-/*
-    dfloat deltael = 0.0;
-    for (dlong i = 0; i < mesh->Nelements; i++) {
-       deltael += tmp[i];
-    }
-    MPI_Allreduce(MPI_IN_PLACE, &deltael, 1, MPI_DFLOAT, MPI_SUM, platform->comm.mpiComm);
-    dlong N = mesh->Nelements;
-    MPI_Allreduce(MPI_IN_PLACE, &N, 1, MPI_DLONG, MPI_SUM, platform->comm.mpiComm);
-    deltael /= N;
-    if(platform->comm.mpiRank == 0)
-      printf("Average element length = %.4e\n",deltael);
-  }
-*/
-
-// Recommended value is epsilon = 1.5/(lx1-1.0), e.g.
-//   polynomial order 5: 0.3
-//   polynomial order 7: 0.2143
-// etc.
-// TODO: calculate from actual polynomial order (mesh->Np) rather than hardcoding.
-const double eps_cls = 0.2143;
-
 // FLiBe(625C,1bar)/Ar(625C,1bar)/3.12mm/0.29 m/s
 static double Re = 231.4;
 static double Sc = 1150;
@@ -36,6 +11,19 @@ static double nuratio = muratio/rhoratio;
 static double diffratio = 1.755e5;
 static double solubilityratio = 356.7;
 
+// Common case information struct, to make device kernel arguments
+// more readable.
+typedef struct caseinfo {
+    // Number of elements local to this CPU node
+    dlong Nelements;
+    // Offset between vector field components
+    dlong fieldOffset;
+    // Element length
+    dfloat deltael;
+    // Target conservative level set (CLS) interface width (epsilon)
+    dfloat eps_cls;
+} caseinfo_t;
+
 /**
  * Apply the smoothed Heaviside step function to input phi, mapping
  * (-infty, infty) to (0, 1), with thickness epsilon.
@@ -44,7 +32,7 @@ static double solubilityratio = 356.7;
  * @param epsilon step function width
  * @returns output between 0 and 1
  */
-inline double heaviside(double phi, double epsilon) {
+inline double heaviside(double phi, double deltael, double epsilon) {
     return 0.5*(std::tanh(phi/(2.0*deltael*epsilon))+1.0);
 }
 
