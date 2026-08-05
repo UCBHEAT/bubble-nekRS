25a560bafe163dd3800071ccdec1c86523129fd2
Benjamin James Li <ben_li@berkeley.edu>
2026-08-05 22:02:04 +0000

    gpu/sherwood: updates to match Nek5000 case params exactly

--- gpu/sherwood/case.hpp
+++ gpu/sherwood/case.hpp
@@ -2,7 +2,7 @@
 static double Re = 231.4;
 static double Sc = 1150;
 static double Pe = Re*Sc;
-static double Fr = 1.633;
+static double Fr = 1.2779; // wrong (sqrt'd) Fr to match Nek5000 - correct one is 1.633;
 static double We = 2.667;
 static double rhoratio = 1.0/0.0002709;
 static double muratio = 148.1;
