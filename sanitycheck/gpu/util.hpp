f077e9c2bdf15463faff9514fca5edcafda51391
Benjamin James Li <ben_li@berkeley.edu>
2026-09-22 10:56:10 -0700

    Commit sanitycheck cases for Nadish's review

---
+++ sanitycheck/gpu/util.hpp
@@ -0,0 +1,14 @@
+// Common case information struct, to make device kernel arguments
+// more readable.
+typedef struct caseinfo {
+    // Number of elements local to this MPI rank.
+    dlong Nelements;
+    // Offset between vector field components.
+    dlong fieldOffset;
+    // Total liquid volume.
+    dfloat liquid_volume;
+    // Total gas volume.
+    dfloat gas_volume;
+    // Global average density, for use in pressure gradient force term.
+    dfloat rho_average;
+} caseinfo_t;
